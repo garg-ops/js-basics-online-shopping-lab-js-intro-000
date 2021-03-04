05a4bea798f7d960b4a6f65da7a76b9d6cd73550
Nilesh Garg <nileshgarg0023@gmail.com>
2021-03-04 06:00:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,38 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+
+ var price = Math.floor(Math.random()*100) + 1
+ var newarr = {itemName:`${item}`, itemPrice: price}
+ cart.push(newarr)
+
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  const temp = [];
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.'
+  }
+  else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0]["itemName"]} at $${cart[0]["itemPrice"]}.`
+  }
+  else {
+    for (let i = 0; i < cart.length - 1; i++) {
+      temp.push(`${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}`)
+    }
+  }
+  return `In your cart, you have ${temp.join(', ')}` + `, and ${cart[cart.length-1]["itemName"]} at $${cart[cart.length-1]["itemPrice"]}.`
 }
 
 function total() {
   // write your code here
+  var t = 0;
+  for (let i = 0; i < cart.length; i++) {
+    t += cart[i]["itemPrice"];
+  }
+  return t
 }
 
 function removeFromCart(item) {
