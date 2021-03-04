057a1c8627dc52ded0cb4af10b11746a27bd9d95
Nilesh Garg <nileshgarg0023@gmail.com>
2021-03-04 06:01:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for (var i = 0; i < cart.length; i++) {
-    if (cart[i]) {
+    if (cart[i].itemName === item) {
 
     }
   }
